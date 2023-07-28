b0012f0d517163e193b563da9f56d5140e078c8e
Emir <emirsurmen@gmail.com>
2023-07-28 16:16:44 +0300

    Make button disabled when updating username

--- lib/widgets/account_page/change_username.dart
+++ lib/widgets/account_page/change_username.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
@@ -23,65 +24,90 @@ class ChangeUsername extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomButton(
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            Widget usernameButtonChild = BlocBuilder(
-              bloc: bloc,
-              builder: (BuildContext context, state) {
-                if (state is UsernameLoading) {
-                  return const CupertinoActivityIndicator(color: Colors.black);
-                } else if (state is UsernameSetError) {
-                  return initialChild;
-                } else if (state is UsernameSetSuccess) {
-                  Navigator.pop(context);
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    Get.snackbar(
-                      "Success!",
-                      "Your username has been changed to ${controller.text}",
-                      colorText: Colors.white,
-                      icon: const Icon(Icons.verified_outlined, color: Colors.green)
-                    );
-                  });
+    return StatefulBuilder(
+      builder: (context, StateSetter setState) {
+        bool enabled = true;
 
-                  return initialChild;
-                }
+        return CustomButton(
+          onPressed: () {
+            HapticFeedback.lightImpact();
 
-                return initialChild;
-              },
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                Widget usernameButtonChild = BlocBuilder(
+                  bloc: bloc,
+                  builder: (BuildContext context, state) {
+                    if (state is UsernameLoading) {
+                      enabled = false;
+                      return const CupertinoActivityIndicator(color: Colors.black);
+                    } else if (state is UsernameSetError) {
+                      Get.snackbar(
+                        "Error!",
+                        "We couldn't change your username.",
+                        colorText: Colors.white,
+                        icon: const Icon(Icons.warning_amber, color: Colors.red),
+                        shouldIconPulse: false
+                      );
+                      enabled = true;
+                      return initialChild;
+                    } else if (state is UsernameSetSuccess) {
+                      Navigator.pop(context);
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        Get.snackbar(
+                          "Success!",
+                          "Your username has been changed to ${controller.text}",
+                          colorText: Colors.white,
+                          icon: const Icon(Icons.verified_outlined, color: Colors.green)
+                        );
+                      });
+
+                      return initialChild;
+                    }
+
+                    return initialChild;
+                  },
+                );
+
+                return AlertDialog(
+                  title: const Text("Change username", style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: geologicaBold,
+                    fontSize: 18
+                  )),
+                  content: CustomTextField(
+                    hintText: "Type in an username",
+                    controller: controller,
+                    keyboardType: TextInputType.text,
+                    maxLength: 14,
+                  ),
+                  actions: [
+                    CustomButton(
+                      onPressed: () => Navigator.pop(context),
+                      backgroundColor: Colors.white.withOpacity(.9),
+                      foregroundColor: Colors.black87,
+                      text: "Cancel",
+                    ),
+                    CustomButton(
+                      onPressed: () {
+                        if (enabled) {
+                          bloc.add(SetUsernameOfUser(username: controller.text));
+                        }
+                      },
+                      backgroundColor: Colors.white.withOpacity(.9),
+                      foregroundColor: Colors.black87,
+                      child: usernameButtonChild,
+                    )
+                  ],
+                );
+              }
             );
-
-            return AlertDialog(
-              title: const Text("Change username"),
-              content: CustomTextField(
-                hintText: "Type in an username",
-                controller: controller,
-                keyboardType: TextInputType.text,
-                maxLength: 14,
-              ),
-              actions: [
-                CustomButton(
-                  onPressed: () => Navigator.pop(context),
-                  backgroundColor: Colors.white.withOpacity(.9),
-                  foregroundColor: Colors.black87,
-                  text: "Cancel",
-                ),
-                CustomButton(
-                  onPressed: () => bloc.add(SetUsernameOfUser(username: controller.text)),
-                  backgroundColor: Colors.white.withOpacity(.9),
-                  foregroundColor: Colors.black87,
-                  child: usernameButtonChild,
-                )
-              ],
-            );
-          }
+          },
+          backgroundColor: Colors.white.withOpacity(.9),
+          foregroundColor: Colors.black87,
+          text: "Change Username",
         );
-      },
-      backgroundColor: Colors.white.withOpacity(.9),
-      foregroundColor: Colors.black87,
-      text: "Change Username",
+      }
     );
   }
 }
