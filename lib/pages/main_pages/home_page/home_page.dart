2b3fa49c6f9972f9bcb4df2738a8ffc830c5aab0
Emir <emirsurmen@gmail.com>
2023-07-31 11:07:05 +0300

    Remove contacts list from home page

--- lib/pages/main_pages/home_page/home_page.dart
+++ lib/pages/main_pages/home_page/home_page.dart
@@ -1,6 +1,3 @@
-import 'package:contacts_service/contacts_service.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:scribble/widgets/home_page/camera_view.dart';
@@ -15,13 +12,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late Future<List<Contact>> contacts;
 
   @override
   void initState() {
     super.initState();
     requestContactPermission();
-    contacts = getAvailableContacts();
   }
 
   requestContactPermission() async {
@@ -38,59 +33,15 @@ class _HomePageState extends State<HomePage> {
           color: Colors.black87
         ),
         padding: const EdgeInsets.all(15),
-        child: Column(
+        child: const Column(
           children: [
-            const HomeTopBar(),
-            const SizedBox(height: 15),
-            const CameraView(),
-            const SizedBox(height: 10),
-            Expanded(
-              child: FutureBuilder(
-                future: contacts,
-                builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                  if (snapshot.connectionState != ConnectionState.done) {
-                    return const Center(
-                      child: CupertinoActivityIndicator(),
-                    );
-                  }
-
-                  List<Contact> contacts = snapshot.data;
-
-                  if (contacts.isEmpty) {
-                    return const Center(
-                      child: Text("You don't have any contacts!"),
-                    );
-                  }
-
-                  return ListView.builder(
-                    padding: EdgeInsets.zero,
-                    itemCount: contacts.length,
-                    shrinkWrap: true,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Text(contacts[index].displayName!);
-                    },
-                  );
-                }
-              ),
-            )
+            HomeTopBar(),
+            SizedBox(height: 15),
+            CameraView(),
+            SizedBox(height: 10),
           ],
         ),
       ),
     );
-  }
-
-  Future<List<Contact>> getAvailableContacts() async {
-    ListResult dbUsers = await FirebaseStorage.instance.ref().child("users/").list();
-    List<Contact> availableContacts = [];
-
-    for (var user in dbUsers.prefixes) {
-      var contact = (await ContactsService.getContactsForPhone(user.name));
-
-      if (contact.isNotEmpty) {
-        availableContacts.add(contact[0]);
-      }
-    }
-
-    return availableContacts;
   }
 }
