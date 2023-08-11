cfd1575f64e60058972fc80a8d3e54897e1d1920
Emir <emirsurmen@gmail.com>
2023-08-11 22:03:24 +0300

    Fix imports order

--- lib/widgets/account_page/change_pfp.dart
+++ lib/widgets/account_page/change_pfp.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:image_cropper/image_cropper.dart';
 
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
