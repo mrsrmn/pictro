71f27bdd6fc4b61b6e4b72989c7e1b6daef2a995
Emir <emirsurmen@gmail.com>
2023-08-28 17:38:43 +0300

    Get ready for release

--- lib/pages/register/register.dart
+++ lib/pages/register/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -14,14 +15,24 @@ import 'package:pictro/utils/auth.dart';
 import 'package:pictro/utils/constants.dart';
 import 'package:pictro/injection_container.dart';
 import 'package:pictro/bloc/register/register_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
-class Register extends StatelessWidget {
-  Register({super.key});
+class Register extends StatefulWidget {
+  const Register({super.key});
 
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
   final TextEditingController _controller = TextEditingController();
+
   final Region region = Region();
+
   final RegisterBloc bloc = sl<RegisterBloc>();
+
+  bool checkedValue = false;
 
   @override
   Widget build(BuildContext context) {
@@ -40,9 +51,54 @@ class Register extends StatelessWidget {
                 const SizedBox(height: 10),
                 PhoneField(controller: _controller),
                 const SizedBox(height: 10),
+                CheckboxListTile(
+                  title: ExcludeSemantics(
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: "I have read the ",
+                          ),
+                          TextSpan(
+                            text: "Privacy Policy",
+                            style: const TextStyle(
+                                color: Colors.blue
+                            ),
+                            recognizer: TapGestureRecognizer()..onTap = () {
+                              launchUrl(Uri.parse(
+                                "https://www.freeprivacypolicy.com/live/3ade9efb-65ab-4616-a478-a379e81721a7"
+                              ));
+                            }
+                          ),
+                          const TextSpan(
+                            text: " and accept it.",
+                          )
+                        ]
+                      ),
+                    ),
+                  ),
+                  value: checkedValue,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      checkedValue = value ?? false;
+                    });
+                  },
+                  controlAffinity: ListTileControlAffinity.leading,
+                ),
+                const SizedBox(height: 10),
                 CustomButton(
                   onPressed: () {
                     HapticFeedback.lightImpact();
+
+                    if (!checkedValue) {
+                      Get.snackbar(
+                        "Error!",
+                        "You need to accept our Privacy Policy in order to use Pictro!",
+                        colorText: Colors.white,
+                        icon: const Icon(Icons.warning_amber, color: Colors.red),
+                      );
+                      return;
+                    }
                     bloc.add(RegisterValidateNumber(value: _controller.text));
                   },
                   backgroundColor: Colors.purple.shade500,
