48360f5f87d9d90bbcdc1a4591f93f8b1f4df043
Emir <emirsurmen@gmail.com>
2023-09-10 12:44:41 +0300

    Fix Android widget image

--- android/app/src/main/kotlin/com/emirs/pictro/HomeWidgetProvider.kt
+++ android/app/src/main/kotlin/com/emirs/pictro/HomeWidgetProvider.kt
@@ -21,23 +21,8 @@ class HomeWidgetProvider : HomeWidgetProvider() {
                 )
                 setOnClickPendingIntent(R.id.widget_container, pendingIntent)
 
-                val scribbUrl: String? = "https://firebasestorage.googleapis.com/v0/b/scribble-98cba.appspot.com/o/images%2F%2B905323314939%2F1694010626416.png?alt=media&token=94f81179-f039-4e40-a4ba-d7a7d51d2792"
+                val scribbUrl: String? = widgetData.getString("pictr_url", null)
 
-                Thread {
-                    val views = this
-                    val bitmap: Bitmap = Picasso.get().load(scribbUrl).resize(
-                        500, 500
-                    ).get()
-
-                    views.setViewVisibility(R.id.scribb_view, View.VISIBLE)
-                    views.setViewVisibility(R.id.default_text, View.GONE)
-                    views.setImageViewBitmap(R.id.scribb_view, bitmap)
-
-                    appWidgetManager.updateAppWidget(widgetId, views)
-                    bitmap.recycle()
-                }.start()
-
-                /*
                 if (scribbUrl == null) {
                     setViewVisibility(R.id.default_text, View.VISIBLE)
                     setViewVisibility(R.id.scribb_view, View.GONE)
@@ -46,7 +31,7 @@ class HomeWidgetProvider : HomeWidgetProvider() {
                     Thread {
                         val views = this
                         val bitmap: Bitmap = Picasso.get().load(scribbUrl).resize(
-                        500, 500
+                            500, 500
                         ).get()
 
                         views.setViewVisibility(R.id.scribb_view, View.VISIBLE)
@@ -57,7 +42,6 @@ class HomeWidgetProvider : HomeWidgetProvider() {
                         bitmap.recycle()
                     }.start()
                 }
-                 */
             }
 
             appWidgetManager.updateAppWidget(widgetId, views)
