fd6e0b6812ea03d45503bdd1b195eddfac452ce0
Emir <emirsurmen@gmail.com>
2023-09-12 14:45:17 +0300

    Update version (1.0.2)

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 const String geologicaRegular = "Geologica-Regular";
 const String geologicaMedium = "Geologica-Medium";
 const String geologicaBold = "Geologica-Bold";
-const String kIOSVersion = "1.0.1";
+const String kIOSVersion = "1.0.2";
 const String kAndroidVersion = "1.0.0";
